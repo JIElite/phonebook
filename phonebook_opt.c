b06f830a00e7c3c0b638a6ce6004364e81092ca6
JIElite <ita3051@gmail.com>
2015-10-10 15:22:55 +0800

    reduce structure, using hashing with static table which may not be uniform
    Reduce strucure:
    reduce structure by moving details to another structure named __PHONE_DATAILS,
    in order to reduce the size of structure. By doing so, the structure is small
    enough to be put into L1-cache.
    
    Hashing:
    I use a prime number 2377 as hash table size and the hash table is consturct
    by array. In other words, this hash table is static, and NOT uniform. it may
    waste some additional space.
    
    The following are some performance infomeasured by linux tool: perf
    $ perf stat --repeat 10 -e cache-misses,cache-references,instructions ,cycles
    ./phonebook_opt
    
        size of entry : 32 bytes
        execution time of append() : 0.044819 sec
        execution time of findName() : 0.000001 sec
    
        Performance counter stats for './phonebook_opt' (10 runs):
    
        380,025         cache-misses        #   35.731 % of all cache refs        ( +-  1.54% )
        1,063,566       cache-references                                          ( +-  0.88% )
        228,791,109     instructions        #   1.93  insns per cycle             ( +-  0.08% )
        118,811,837     cycles                                                    ( +-  0.81% )
    
        0.045587579 seconds time elapsed                                          ( +-  0.82% )

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,15 +1,70 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "phonebook_opt.h"
 
-/* FILL YOUR OWN IMPLEMENTATION HERE! */
-entry *findName(char lastname[], entry *pHead)
-{
-    /* TODO: implement */
-    return NULL;
+
+entry *hash_table[HASH_TABLE_SIZE];
+entry *hash_entry[HASH_TABLE_SIZE];
+
+int hash(char lastname[]){
+    int number = 0;
+    while(*lastname){
+        number += *lastname++;
+        if (*lastname) number += ((int) *lastname++) << 8;
+    }
+    return number % HASH_TABLE_SIZE;
 }
 
-entry *append(char lastName[], entry *e)
+/*
+int hash(char lastname[]){
+    const unsigned char *name = (const unsigned char *)lastname;
+    unsigned int h = 5381;
+    int ch;
+
+    while ((ch = *name++) != '\0')
+        h = (h << 5) + h + ch;
+    
+    return h % HASH_TABLE_SIZE;
+}
+*/
+
+entry *findName(char lastname[], entry *pHead)
 {
+    int hash_value = hash(lastname);
+//    int hash_value = lastname[0] - 'a';
+    pHead = hash_table[hash_value];
+
+    while (pHead) {
+        if (strcmp(pHead->lastName, lastname) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
+        
     return NULL;
+
+}
+
+
+entry *append(char lastname[], entry *e)
+{
+    int hash_value = hash(lastname);
+//    int hash_value = lastname[0] - 'a';
+    entry *new_node = (entry *)malloc(sizeof(entry));
+
+    if (hash_table[hash_value] == NULL){
+        new_node->pNext = NULL;
+        strcpy(new_node->lastName, lastname);
+        hash_table[hash_value] = new_node;
+        hash_entry[hash_value] = new_node;
+        return new_node;
+    }
+    
+    hash_entry[hash_value]->pNext = new_node;
+    strcpy(new_node->lastName, lastname);
+    new_node->pNext = NULL;
+    hash_entry[hash_value] = new_node;
+    return new_node;
 }
--- phonebook_opt.h
+++ phonebook_opt.h
@@ -2,10 +2,11 @@
 #define _PHONEBOOK_H
 
 #define MAX_LAST_NAME_SIZE 16
+#define HASH_TABLE_SIZE 2377 
 
-/* original version */
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+struct __PHONE_DETAILS{
+    char state[2];
+    char zip[5];
     char firstName[16];
     char email[16];
     char phone[10];
@@ -13,11 +14,21 @@ typedef struct __PHONE_BOOK_ENTRY {
     char addr1[16];
     char addr2[16];
     char city[16];
-    char state[2];
-    char zip[5];
-    struct __PHONE_BOOK_ENTRY *pNext;
+};
+
+
+/* original version */
+typedef struct __PHONE_LAST_NAME {
+    char lastName[MAX_LAST_NAME_SIZE];
+    struct __PHONE_DETAILS *detail;
+    struct __PHONE_LAST_NAME *pNext;
 } entry;
 
+extern entry *hash_table[HASH_TABLE_SIZE];
+extern entry *hash_entry[HASH_TABLE_SIZE];
+
+
+int hash(char lastname[]);
 entry *findName(char lastname[], entry *pHead);
 entry *append(char lastName[], entry *e);
 
